e5da82f2d8156cc3bbf28d774bf8a3715ab51455
GuaiYiHu <guaiyihu@foxmail.com>
2018-04-05 22:00:03 +0800

    lavender: Clean up unused configs in device.mk
    Change-Id: Ib68ba04da7641b4fa7387a29a9177109135fadaa

--- device.mk
+++ device.mk
@@ -1,4 +1,8 @@
-TARGET_USES_AOSP := true
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
@@ -13,27 +17,7 @@ ifeq ($(ENABLE_VENDOR_IMAGE), true)
 #TARGET_USES_QTIC := false
 endif
 
-TARGET_USES_AOSP_FOR_AUDIO := false
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-TARGET_DISABLE_DASH := true
-
-TARGET_KERNEL_VERSION := 4.4
-BOARD_FRP_PARTITION_NAME := frp
-TARGET_USES_NQ_NFC := true
-
-# enable the SVA in UI area
-TARGET_USE_UI_SVA := true
-
-TARGET_USES_MKE2FS := true
-#QTIC flag
--include $(QCPATH)/common/config/qtic-config.mk
-
-# Add soft home, back and multitask keys
-PRODUCT_PROPERTY_OVERRIDES += \
-    qemu.hw.mainkeys=0
-
 # Video codec configuration files
-ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
@@ -53,10 +37,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.video.disable.ubwc=1
 
-ifneq ($(TARGET_DISABLE_DASH), true)
-    PRODUCT_BOOT_JARS += qcmediaplayer
-endif
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service \
@@ -66,21 +46,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, device/qcom/common/common64.mk)
 
 # default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
-
-# When can normal compile this module,  need module owner enable below commands
-# font rendering engine feature switch
-#-include $(QCPATH)/common/config/rendering-engine.mk
-#ifneq (,$(strip $(wildcard $(PRODUCT_RENDERING_ENGINE_REVLIB))))
-#    MULTI_LANG_ENGINE := REVERIE
-#    MULTI_LANG_ZAWGYI := REVERIE
-#endif
-
-# WLAN chipset
-WLAN_CHIPSET := qca_cld3
 
 #
 # system prop for opengles version
@@ -89,16 +57,10 @@ WLAN_CHIPSET := qca_cld3
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
 
-#Android EGL implementation
-PRODUCT_PACKAGES += libGLES_android
 PRODUCT_BOOT_JARS += tcmiface
 PRODUCT_BOOT_JARS += telephony-ext
 
 PRODUCT_PACKAGES += telephony-ext
-
-ifneq ($(strip $(QCPATH)),)
-PRODUCT_BOOT_JARS += WfdCommon
-endif
 
 # system prop for Bluetooth SOC type
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -111,9 +73,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/qcom/common/vendor_framewor
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(LOCAL_PATH)/vendor_framework_compatibility_matrix.xml
 
-# Audio configuration file
--include $(TOPDIR)hardware/qcom/audio/configs/sdm660/sdm660.mk
-
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # Sensor HAL conf file
@@ -122,11 +81,6 @@ PRODUCT_COPY_FILES += \
 # Exclude TOF sensor from InputManager
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
-
-# WLAN host driver
-ifneq ($(WLAN_CHIPSET),)
-PRODUCT_PACKAGES += $(WLAN_CHIPSET)_wlan.ko
-endif
 
 # WLAN driver configuration file
 PRODUCT_COPY_FILES += \
@@ -230,13 +184,6 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OU
 # MSM IRQ Balancer configuration file for SDM630
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/msm_irqbalance_sdm630.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm630.conf
 
-# dm-verity configuration
-PRODUCT_SUPPORTS_VERITY := true
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
-ifeq ($(ENABLE_VENDOR_IMAGE), true)
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/bootdevice/by-name/vendor
-endif
-
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 PRODUCT_VENDOR_MOVE_ENABLED := true
@@ -244,11 +191,6 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 #for android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
-
-# Add the overlay path
-#PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
-#       $(QCPATH)/qrdplus/globalization/multi-language/res-overlay \
-#      $(PRODUCT_PACKAGE_OVERLAYS)
 
 #for wlan
 PRODUCT_PACKAGES += \
