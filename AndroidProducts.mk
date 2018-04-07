78c96e059d32d5189d776d2a8502d811df5b4ae8
Dan Willemsen <dwillemsen@google.com>
2018-04-07 15:15:48 -0700

    jasmine: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: I7f06edb331a2a56f29db26294b2ece28666d2103
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -24,3 +24,8 @@
 # inherit MI A2 HAVOC device config
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_jasmine_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_jasmine_sprout-eng \
+    havoc_jasmine_sprout-userdebug \
+		havoc_jasmine_sprout-eng
