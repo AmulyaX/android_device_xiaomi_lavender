7fe34f6dd08a0b147b6410bb59ba3282d36374b4
Dan Willemsen <dwillemsen@google.com>
2018-06-19 22:28:20 -0700

    lavender: build have overriding commands
    These products have warnings about overriding build commands, which
    means that there are multiple instructions on how to build a file, and
    we've just picked one of them. Which one we pick is usually stable, but
    it can change.
    Bug: 77611511
    Test: turn default to false, walleye still uses a warning
    Change-Id: I11d194863b313f64b19291f65219682b169d9ced

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
 TARGET_USES_64_BIT_BINDER := true
+BUILD_BROKEN_DUP_RULES := true
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
