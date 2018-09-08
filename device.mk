747d5557d12aeb8a6a4f197bc56377def91f0cfa
dianlujitao <dianlujitao@lineageos.org>
2018-09-08 18:19:12 +0800

    lavender: Build vendor android.hidl.base@1.0.so
    Change-Id: I39c71e61252228dbfcf50047795ea23b2275e4bd
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

--- device.mk
+++ device.mk
@@ -242,10 +242,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service \
     chargeonlymode
 
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_vendor \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
