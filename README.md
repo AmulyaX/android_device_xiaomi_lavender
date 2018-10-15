d4dab9276e12de6f6b55a8ad1a944f4d848c1e27
Max Weffers <rcstar6696@gmail.com>
2018-10-15 11:48:10 +0200

    wayne: Intial Device-Tree for Mi 6X

---
+++ README.md
@@ -0,0 +1,25 @@
+Copyright (C) 2018 CarbonRom
+
+The MI 6X (codenamed _"wayne"_) is a mid-range phone from Xiaomi.
+
+Xiaomi MI 6X was announced and released in July 2018.
+
+## Device specifications
+
+| Device       | Xiaomi MI 6X         |
+| -----------: | :---------------------------------------------- |
+| SoC          | Qualcomm SDM660 Snapdragon 660                  |
+| CPU          | 8x Qualcomm® Kryo™ 260 up to 2.2GHz             |
+| GPU          | Adreno 512                                      |
+| Memory       | 3GB / 4GB / 6GB RAM (LPDDR4X)                   |
+| Shipped Android version | 8.1.0                           	 |
+| Storage      | 32/64/128GB eMMC 5.1 flash storage              |
+| Battery      | Non-removable Li-Po 3010 mAh               	 |
+| Dimensions   | 158.7 x 75.4 x 7.3 mm	                         |
+| Display      | 1080x2160 pixels, 5.99    						 |
+Rear Camera    | 12MP+20MP, AI Dual Camera
+Front Camera   | 20 MP
+
+## Device picture
+
+![Xiaomi MI 6X](https://cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/a4842f9afc615164af83c2088a412fc0.jpg "Xiaomi MI 6X")
---
+++ device.mk
@@ -0,0 +1,45 @@
+#
+ # Copyright 2018 The Android Open Source Project
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+
+ # Inherit the fusion-common definitions
+ $(call inherit-product, device/xiaomi/sdm660-common/platform.mk)
+
+ # setup dalvik vm configs
+ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+ # setup HWUI configs
+ $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+
+ DEVICE_PATH := device/xiaomi/wayne
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
+# Set those variables here to overwrite the inherited values.
+ PRODUCT_NAME := aosp_wayne
+ PRODUCT_DEVICE := wayne
+ PRODUCT_MODEL := Mi 6X (AOSP)
+ PRODUCT_BRAND := Xiaomi
+ PRODUCT_MANUFACTURER := Xiaomi
