1e9e55638620fa152301c2c7f09c0aecc5dbc0f7
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    lavender: Enforce RRO for all resource overlays
    This configuration will likely go in mainline_system configuration.
    
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -17,8 +17,7 @@ DEVICE_PACKAGE_OVERLAYS := \
 	$(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Audio
 PRODUCT_PACKAGES += \
