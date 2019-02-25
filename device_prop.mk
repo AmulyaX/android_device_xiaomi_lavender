d798680eb8a256cf1e1130f10c14e6b86a4c0767
Max Weffers <rcstar6696@gmail.com>
2019-02-25 16:52:36 +0100

    jasmine_sprout: delete wrong config for noise cancelation

--- device_prop.mk
+++ device_prop.mk
@@ -29,7 +29,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.audio.sdk.fluencetype=none \
 	persist.vendor.audio.fluence.voicecall=true \
 	persist.vendor.audio.fluence.voicerec=false \
 	persist.vendor.audio.fluence.speaker=true \
