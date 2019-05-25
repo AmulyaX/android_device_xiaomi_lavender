af41520dd2c733b8fbd2520bc1761b476590d934
Manish4586 <manish.n.manish45@gmail.com>
2019-05-25 21:08:54 +0530

    jasmine: device_prop: remove unused vibrator prop

--- device_prop.mk
+++ device_prop.mk
@@ -116,9 +116,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.iwlan.enable=true \
 	persist.vendor.radio.data_con_rprt=1
-
-# Vibration
-PRODUCT_PROPERTY_OVERRIDES += \
-	sys.haptic.down.normal=5,15 \
-	sys.haptic.down.strong=0,20 \
-	sys.haptic.down.weak=9,11
