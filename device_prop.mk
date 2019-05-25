00fcfadf759bdca1649f83936eee111669311919
Manish4586 <Manish.n.manish45@gmail.com>
2019-05-25 21:31:11 +0530

    device_prop: add stock dpi values
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- device_prop.mk
+++ device_prop.mk
@@ -100,6 +100,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.radio.multisim.config=dsds
 
+# DPI
+PRODUCT_PROPERTY_OVERRIDES += \
+	vendor.display.lcd_density=480
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 	vendor.mm.enable.qcom_parser=262029
