c51e66e87167c65f4d60726b53e7017d8ad16ab0
Manish4586 <Manish.n.manish45@gmail.com>
2019-06-07 21:17:53 +0800

    jasmine: checkout device identification from stock
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- havoc_jasmine_sprout.mk
+++ havoc_jasmine_sprout.mk
@@ -28,15 +28,19 @@ $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 # Inherit from Havoc custom product configuration
 $(call inherit-product, vendor/havoc/config/common.mk)
 
-TARGET_VENDOR_PRODUCT_NAME := jasmine_sprout
-
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.havoc.maintainer=rcstar6696
 
 # Device identifier
-PRODUCT_BRAND := Xiaomi
+PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PLATFORM := SDM660
 PRODUCT_NAME := havoc_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
-PRODUCT_MODEL := MI A2
+PRODUCT_MODEL := Mi A2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="jasmine_sprout" \
+    PRODUCT_NAME="jasmine"
+
+TARGET_VENDOR_PRODUCT_NAME := jasmine
