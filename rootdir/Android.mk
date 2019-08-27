545aa81bce5ad20c3755adfcdecd215dd984c9ca
dianlujitao <dianlujitao@lineageos.org>
2019-08-27 14:22:15 +0530

    lavender: Add init.panel_info.sh
    *And move to vendor/bin
    
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -107,3 +107,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.sensors.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.panel_info.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.panel_info.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
