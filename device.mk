d8d23f10c0ac948c4dbfa1ee2505a8bb4c08cd3b
Bruno Martins <bgcngm@gmail.com>
2019-08-27 17:39:57 +0530

    lavender: Move chargeonlymode
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- device.mk
+++ device.mk
@@ -212,7 +212,9 @@ PRODUCT_COPY_FILES += \
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
+
 
 # HIDL
 PRODUCT_PACKAGES += \
