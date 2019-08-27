a6daeeb2ebe1008f9fc06f8cb5aecd972543ec90
AmulyaX <amulya.b520@gmail.com>
2019-08-27 17:30:38 +0530

    lavender: Import vendor prop from MIUI 10.3.6
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 
 $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
