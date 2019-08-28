d938c5113ea1eca162f7926d16647c996427d4d9
AmulyaX <amulya.b520@gmail.com>
2019-08-28 14:37:02 +0530

    lavender: board: Set TARGET_USES_64_BIT_BINDER as true
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,8 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
+TARGET_USES_64_BIT_BINDER := true
+
 -include $(QCPATH)/common/sdm660_64/BoardConfigVendor.mk
 
 # Some framework code requires this to enable BT
