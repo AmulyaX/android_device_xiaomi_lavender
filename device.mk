83ca48d2a60286f9e7b7116d07bcb2b57ba2c325
AmulyaX <amulya.b520@gmail.com>
2019-08-28 19:08:21 +0530

    lavender: device: Remove duplicate entry for Power
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- device.mk
+++ device.mk
@@ -100,11 +100,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service \
-    android.hardware.power@1.0-impl
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
