13f2c20ed0d2b0bb2c57ca1143de05d05b16c4d3
Arian <arian.kulmer@web.de>
2019-09-08 18:05:06 +0200

    lavender: Uprev clearkey to 1.2

--- device.mk
+++ device.mk
@@ -166,7 +166,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service 
+    android.hardware.drm@1.0-service \
+	android.hardware.drm@1.2-service.clearkey
 
 # Exclude TOF sensor from InputManager
 PRODUCT_COPY_FILES += \
