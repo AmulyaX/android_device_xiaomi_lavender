a4254e67b978d9c3c8a31a3c9388e7e58a801f8c
AmulyaX <amulya.b520@gmail.com>
2019-09-16 23:12:55 +0530

    lavender: board: Specify Vendor Security Patch Level
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,6 +191,9 @@ TARGET_RIL_VARIANT := caf
 # Seccomp
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2018-06-05
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
