30726e1ca1c18f2c3a4140ce0c660b6e01f7f11a
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    lavender: Convert remaining configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -90,7 +90,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=4096
 	
 # Codec2 switch
 PRODUCT_PROPERTY_OVERRIDES += \
