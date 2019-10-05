c67642d06654337ccc8a2cb1ec7b9f2f9f7d6f32
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    lavender: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- device.mk
+++ device.mk
@@ -338,7 +338,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
