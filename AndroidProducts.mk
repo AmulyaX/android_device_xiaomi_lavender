PixelExperience-Devices/device_xiaomi_lavender

lavender: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_lavender-user \
+    aosp_lavender-userdebug \
+    aosp_lavender-eng

AmulyaX/android_device_xiaomi_lavender

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
Signed-off-by: AmulyaX <amulya.b520@gmail.com>

AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng