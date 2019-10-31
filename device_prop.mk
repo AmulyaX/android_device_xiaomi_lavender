e17822a123fec3e0d2d6ad50ac36cbf60db21c0b
Sebita <kjjjnob.seba@gmail.com>
2019-10-31 18:27:40 +0000

    lavender: Set 403 for lcd_density
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

--- device_prop.mk
+++ device_prop.mk
@@ -100,7 +100,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DPI
 PRODUCT_PROPERTY_OVERRIDES += \
-	vendor.display.lcd_density=480
+	vendor.display.lcd_density=403
 
 # Paper mode
 PRODUCT_PROPERTY_OVERRIDES += \
