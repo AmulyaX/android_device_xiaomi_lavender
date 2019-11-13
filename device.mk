7c146793459fdb84f471fedff6a77593dd96b765
AmulyaX <amulya.b520@gmail.com>
2019-11-13 12:05:04 +0530

    lavender: Nuke Doze
    Signed-off-by: AmulyaX <amulya.b520@gmail.com>

--- device.mk
+++ device.mk
@@ -176,10 +176,6 @@ RODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service \
     android.hardware.broadcastradio@1.0-impl
 
-# Doze
-#PRODUCT_PACKAGES += \
-#    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
